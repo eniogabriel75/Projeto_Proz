bad04051b841298248fdb4c5955a1b8805005295
Nicolas Henrique <nicolashenrique.barn@gmail.com>
2024-01-13 12:10:44 -0300

    atualização design

--- JS/contato.js
+++ JS/contato.js
@@ -1,15 +1,15 @@
 
-function abrirModal(abrirModal){
-    let modal = document.getElementById(abrirModal);
-    modal.style.display = 'block';
-    document.body.style.overflow = "hidden";
-};
+// function abrirModal(abrirModal){
+//     let modal = document.getElementById(abrirModal);
+//     modal.style.display = 'block';
+//     document.body.style.overflow = "hidden";
+// };
 
-function fecharModal(fecharModal){
-    let modal = document.getElementById(fecharModal);
-    modal.style.display = 'none';
-    document.body.style.overflow = "auto";
-};
+// function fecharModal(fecharModal){
+//     let modal = document.getElementById(fecharModal);
+//     modal.style.display = 'none';
+//     document.body.style.overflow = "auto";
+// };
 
 function toggleMenu() {
     var menu = document.getElementById("menu");
