8ea9e5ce4db78f6f49b9401f1f0bdebfc9814c9b
Enio G <eniogabrielfla@gmail.comm>
2024-01-24 23:22:45 -0300

    Ajuste_final3

--- js/procedimentos.js
+++ js/procedimentos.js
@@ -1,19 +1,50 @@
-const saibaMais = document.querySelector(".saiba-mais");
-const botaoFecharModal = document.querySelector(".fechar-modal");
-const video = document.getElementById("video");
-const modal = document.querySelector(".modal");
-const linkVideo = video.src;
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+const simpleForm = document.querySelector("#simple-form");
 
-function alternarModal() {
-  modal.classList.toggle("aberto");
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+const handleFormSubmit = async (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+
+  if (!name || !email || !phone) {
+    Swal.fire({
+      icon: "error",
+      title: "Erro",
+      text: "Por favor, preencha todos os campos.",
+    });
+    return;
+  }
+  // Usando SweetAlert para a mensagem de confirmação
+  const result = await Swal.fire({
+    icon: "success",
+    title: "Sucesso",
+    text: "Formulário enviado com sucesso!\nEntraremos em contato com você para confirmação.",
+    showConfirmButton: false,
+    timer: 3000,
+  });
+
+  if (result.dismiss === Swal.DismissReason.timer) {
+    toggleModal();
+  }
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", toggleModal);
+});
+
+simpleForm.addEventListener("submit", handleFormSubmit);
+/*Menu pequeno*/
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("active");
 }
-
-saibaMais.addEventListener("click", () => {
-  alternarModal();
-  video.setAttribute("src", linkVideo);
-});
-
-botaoFecharModal.addEventListener("click", () => {
-  alternarModal();
-  video.setAttribute("src", "");
-});
